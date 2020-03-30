66a844f89f64137ad796a7528e18fa81d5fcc069
sergio-garcia <sergio@ginx.com.br>
2020-03-29 23:13:18 -0300

    Debug test

--- index.js
+++ index.js
@@ -1,19 +1,104 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
+const semver = require('semver');
 
+async function get_git_tags(cwd) {
+  let output = '';
+
+  const options = {
+    cwd: cwd || '.',
+    silent: true
+  };
+  options.listeners = {
+    stdout: (data) => {
+      output += data.toString();
+    },
+    stderr: () => {
+    }
+  };
+
+  await exec.exec('git', ['tag', '-l'], options);
+
+  return output;
+}
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    let output = await get_git_tags();
+    let versions = output
+      .split('\n')
+      .map(semver.parse)
+      .filter(l => l != null);
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    versions.sort(semver.compare);
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
+    let version = versions.length ? versions[versions.length - 1] : semver.parse('0.0.0');
+
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+    const context = github.context;
+
+    const { major, minor, patch } = version;
+    const { prereleaseName, prereleaseVersion } = version.prerelease;
+
+    let request;
+
+    core.debug(process.env)
+    core.debug(context)
+    core.debug(context.repo)
+
+    request = await octokit.git.getRef({
+      ...context.repo,
+      ref: context.ref
+    })
+
+    let ref = request.data
+
+    request = await octokit.git.getCommit({
+      ...context.repo,
+      commit_sha: ref.object.sha,
+    });
+
+    let commit = request.data
+
+    core.debug(commit)
+
+    // request = await octokit.git.createTag({
+    //   ...context.repo,
+    //   tag: 'v' + version.version,
+    //   message: version.version,
+    //   object: ref.object.sha,
+    //   type: ref.object.type,
+    // })
+
+    // let tag = request.data
+
+    // request = await octokit.git.createRef({
+    //   owner: context.repo.owner,
+    //   repo: context.repo.repo,
+    //   ref: 'refs/tags/' + tag.tag,
+    //   sha: tag.object.sha,
+    // })
+
+    // console.log(request.data)
+
+    // console.log(context.ref)
+    // console.log(context.repo)
+    // console.log(context.payload)
+
+    // console.log(semver.inc(version, 'major'))
+    // console.log(semver.inc(version, 'minor'))
+    // console.log(semver.inc(version, 'patch'))
+
+    core.setOutput('version', major);
+    core.setOutput('major', major);
+    core.setOutput('minor', minor);
+    core.setOutput('patch', patch);
+    core.setOutput('preName', prereleaseName);
+    core.setOutput('preVersion', prereleaseVersion);
+    // console.log(version);
+  }
   catch (error) {
     core.setFailed(error.message);
   }
